97f424f9b25a1a70ebc4ff92c052ac458b1e0b82
LuancGerlach <luancgerlach@gmail.com>
2025-01-30 12:45:18 -0300

    Add initial integration test configuration

---
+++ src/cars/infrastructure/typeorm/repositories/cars-typeorm.repository.int-spec.ts
@@ -0,0 +1,28 @@
+import { testDataSource } from '@/common/infrastructure/typeorm/testing/data-source'
+import { CarsTypeormRepository } from './cars-typeorm.repository'
+import { Car } from '../entities/cars.entity'
+import { CarsDataBuilder } from '../../testing/helpers/cars-data-builder'
+import { NotFoundError } from '@/common/domain/errors/not-found-error'
+import { CarModel } from '@/cars/domain/models/cars.model'
+
+describe('CarsTypeormRepository integration tests', () => {
+  let ormRepository: CarsTypeormRepository
+
+  beforeAll(async () => {
+    await testDataSource.initialize()
+  })
+
+  afterAll(async () => {
+    await testDataSource.destroy()
+  })
+
+  beforeEach(async () => {
+    await testDataSource.manager.query('DELETE FROM cars')
+    ormRepository = new CarsTypeormRepository()
+    ormRepository.carsRepository = testDataSource.getRepository(Car)
+  })
+
+  describe('method', () => {
+    it('should', () => {})
+  })
+})
--- src/common/infrastructure/typeorm/testing/data-source.ts
+++ src/common/infrastructure/typeorm/testing/data-source.ts
@@ -1,18 +1,16 @@
-import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { env } from '@/common/infrastructure/env'
 
-const testDataSource = new DataSource({
+export const testDataSource = new DataSource({
   type: env.DB_TYPE,
   host: env.DB_HOST,
   port: env.DB_PORT,
-  database: env.DB_NAME,
   username: env.DB_USER,
   password: env.DB_PASS,
+  database: env.DB_NAME,
+  schema: env.DB_SCHEMA,
   entities: ['**/entities/**/*.ts'],
   migrations: ['**/migrations/**/*.ts'],
   synchronize: true,
   logging: true,
 })
-
-export { testDataSource }
